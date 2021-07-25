0bd585055ee97ab1debd8c1159603c8ba9f1e79f
Tobias Rath <tobias.rath@protonmail.com>
2021-07-25 14:12:58 +0200

    Small cleanup

--- main.go
+++ main.go
@@ -49,46 +49,73 @@ type status struct {
 }
 
 func minecraftHandler(c *fiber.Ctx) error {
-	resp, _, err := bot.PingAndList(c.Params("server"))
-	if err != nil {
-		log.Printf("Ping and list failed: %v\n", err)
-		return err
-	}
-
 	var s status
-	err = json.Unmarshal(resp, &s)
+	err := pingServer(c.Params("server"), &s)
 	if err != nil {
-		log.Printf("Unmarshal of ping and list response failed: %v\n", err)
 		return err
 	}
 
+	img, err := getFavicon(&s)
+	if err != nil {
+		return err
+	}
+
+	banner := generateBanner(img, &s)
+
+	c.Set("Content-Type", "image/png")
+	return banner.EncodePNG(c.Response().BodyWriter())
+}
+
+func generateBanner(favicon image.Image, s *status) *gg.Context {
+	dc := gg.NewContext(width, height)
+	dc.SetRGB(0, 0, 0)
+	dc.Clear()
+	dc.SetRGB(1, 1, 1)
+	dc.DrawImage(favicon, 0, 0)
+	drawStringsToContext(dc, s.Description.ClearString())
+	drawStringsToContext(dc, s.Version.Name)
+	drawStringsToContext(dc, fmt.Sprintf("%d/%d", s.Players.Online, s.Players.Max))
+
+	return dc
+}
+
+func getFavicon(s *status) (image.Image, error) {
 	faviconString := strings.Split(s.Favicon, ",")[1]
 	faviconData, err := base64.StdEncoding.DecodeString(faviconString)
 	if err != nil {
 		log.Printf("Unable to decode favicon base64: %v\n", err)
-		return err
+		return nil, err
 	}
 
 	img, _, err := image.Decode(bytes.NewBuffer(faviconData))
 	if err != nil {
 		log.Printf("Decoding of favicon failed: %v\n", err)
+		return nil, err
+	}
+
+	return scaleFavicon(img), nil
+}
+
+func scaleFavicon(old image.Image) image.Image {
+	res := image.NewRGBA(image.Rect(0, 0, height, height))
+	draw.BiLinear.Scale(res, res.Bounds(), old, old.Bounds(), draw.Over, nil)
+	return res
+}
+
+func pingServer(address string, result *status) error {
+	resp, _, err := bot.PingAndList(address)
+	if err != nil {
+		log.Printf("Ping and list failed: %v\n", err)
 		return err
 	}
 
-	scaledImg := image.NewRGBA(image.Rect(0, 0, height, height))
-	draw.BiLinear.Scale(scaledImg, scaledImg.Bounds(), img, img.Bounds(), draw.Over, nil)
+	err = json.Unmarshal(resp, result)
+	if err != nil {
+		log.Printf("Unmarshal of ping and list response failed: %v\n", err)
+		return err
+	}
 
-	dc := gg.NewContext(width, height)
-	dc.SetRGB(0, 0, 0)
-	dc.Clear()
-	dc.SetRGB(1, 1, 1)
-	dc.DrawImage(scaledImg, 0, 0)
-	drawStringsToContext(dc, s.Description.ClearString())
-	drawStringsToContext(dc, s.Version.Name)
-	drawStringsToContext(dc, fmt.Sprintf("%d/%d", s.Players.Online, s.Players.Max))
-
-	c.Set("Content-Type", "image/png")
-	return dc.EncodePNG(c.Response().BodyWriter())
+	return nil
 }
 
 func drawStringsToContext(dc *gg.Context, s string) {
